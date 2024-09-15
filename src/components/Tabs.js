1dc7e2e185212be677082d364de685ae49423d51
Mustafa <pourmhere@gmail.com>
2024-09-16 01:52:19 +0530

    done

---
+++ src/components/Tabs.js
@@ -0,0 +1,24 @@
+import React,{useState} from "react";
+
+const Tabs = ({tabData}) => {
+   const [activeTab, setActiveTab] = useState(tabData[0])
+
+   return (
+      <div>
+           <ul>
+               {
+                  tabData.map((tab)=>(
+                       <li onClick={()=>setActiveTab(tab)}>{tab.title}</li>
+                  ))
+               }
+           </ul>
+           <div>
+               {
+                 <p>{activeTab.content}</p>
+               }
+          </div>
+      </div>
+   )
+}
+
+export default Tabs
